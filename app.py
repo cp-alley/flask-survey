3f31223b511b911bd2b49722acabb4cb44bfd1f8
Christopher Alley <calleyviola@gmail.com>
2023-08-24 19:41:45 -0400

    Add session stuff

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ debug = DebugToolbarExtension(app)
 def show_homepage():
     """Show the home page survey start"""
     session["responses"] = []
+    session["questions_answered"] = {}
     return render_template("survey_start.html",
                            title=survey.title,
                            instructions=survey.instructions)
@@ -26,6 +27,15 @@ def redirect_to_handle_questions():
 @app.get("/questions/<int:num_of_question>")
 def handle_questions(num_of_question):
     '''Shows current question'''
+    questions_answered = session["questions_answered"]
+
+    if num_of_question not in questions_answered:
+        print("num_of_question=", num_of_question, "current_quest", questions_answered)
+        return redirect(f"/questions/{len(questions_answered)}")
+
+    questions_answered.append(num_of_question)
+    session["questions_answered"] = questions_answered
+
     return render_template("question.html",
                            question = survey.questions[num_of_question]) #pull this out into a variable
 
@@ -36,7 +46,7 @@ def handle_answer():
 
     """
     answer = request.form["answer"]
-    
+
     responses = session["responses"]
     responses.append(answer)
     session["responses"] = responses
@@ -52,6 +62,7 @@ def handle_answer():
 def show_completion_page():
     """Show thank you message and survey answers"""
 
+    responses = session["responses"]
     prompts = [q.prompt for q in survey.questions]
     data = {prompts[i]: responses[i] for i in range(len(responses))}
 
